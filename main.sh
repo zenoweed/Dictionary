1d79a42a4a638616289bcde6b573a5fe418fb01d
Brian Nestan Nestan <43124953+zenoweed@users.noreply.github.com>
2022-02-14 04:40:06 +0000

    added regex_wrap + renamed dictionary folder

--- main.sh
+++ main.sh
@@ -3,5 +3,11 @@ echo Dictionary-----------------------------------------------
 
 echo "Input Word:"
 read word
+
+# Wrap word with regex to better match string
+reg_wrap=''^$word\s''
+
+echo $reg_wrap
+
 echo Entries matching: $word
-grep ''$word'' dictionary/*.txt
+grep ''$reg_wrap'' dict/*.txt
